1e5e99e269c5312a438e2067a209316e608a7020
Anson Jablinski <a@ansonj.org>
2023-01-02 09:12:13 -0600

    Implement Game's guess submission and scoring

--- Mastermind/Game.swift
+++ Mastermind/Game.swift
@@ -1,11 +1,11 @@
-struct Game {
+class Game {
     enum Status {
         case inProgress
         case won
         case lost
     }
-    enum GuessError {
-        case gameAlreadyLost
+    enum GuessError: Error {
+        case gameAlreadyOver
     }
     
     static let maxNumberOfGuesses = 10
@@ -23,15 +23,39 @@ struct Game {
     /// Submits a guess to the game history.
     /// Scores the given guess, increments the guess count, and updates the game status.
     func submit(guess: Combination) throws -> (CombinationScore, Status) {
-        // Throw if you're out of guesses
-        // Increment guesses
-        // Update game status
-        // Return the score and status
-        fatalError("Not implemented")
+        guard status == .inProgress else {
+            throw GuessError.gameAlreadyOver
+        }
+        
+        let score = Self.score(guess: guess,
+                               forSolution: solution)
+        guesses.append(Guess(combination: guess,
+                             score: score))
+
+        if guess == solution {
+            status = .won
+        } else if guesses.count >= Self.maxNumberOfGuesses {
+            status = .lost
+        }
+        
+        return (score, status)
     }
     
     /// Returns a score for the given guess and solution.
     static func score(guess: Combination, forSolution solution: Combination) -> CombinationScore {
-        fatalError("Not implemented")
+        precondition(guess.count == solution.count, "Your guess has \(guess.count) pegs versus the solution which has \(solution.count) pegs")
+        
+        var blackScorePegCount = 0
+        var whiteScorePegCount = 0
+        for index in 0..<guess.count {
+            if solution[index] == guess[index] {
+                blackScorePegCount += 1
+            } else if guess.contains(solution[index]) {
+                whiteScorePegCount += 1
+            }
+        }
+        
+        return CombinationScore(blackScorePegs: blackScorePegCount,
+                                whiteScorePegs: whiteScorePegCount)
     }
 }
