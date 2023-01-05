ee1e5a4fa3e184c6c7ef79acf93a1856995e9903
Anson Jablinski <a@ansonj.org>
2023-01-04 21:40:48 -0600

    Include percentages in statistics

--- Mastermind/main.swift
+++ Mastermind/main.swift
@@ -1,3 +1,4 @@
+import Foundation
 let allowDuplicates = false
 let debug = false
 let trials = 1_000
@@ -13,6 +14,15 @@ for _ in 0..<trials {
     trialResults[result, default: 0] += 1
 }
 print("I just played \(trials) games")
+let formatter = {
+    let f = NumberFormatter()
+    f.numberStyle = .percent
+    f.minimumFractionDigits = 1
+    f.maximumFractionDigits = 1
+    f.formatWidth = 6
+    f.paddingPosition = .beforePrefix
+    return f
+}()
 trialResults.keys.sorted(by: { left, right in
     switch (left, right) {
     case (.lost, .lost):
@@ -25,5 +35,8 @@ trialResults.keys.sorted(by: { left, right in
         return leftWon < rightWon
     }
 }).forEach { result in
-    print("\t\(result): \(trialResults[result]!)")
+    let count = trialResults[result]!
+    let percentage = Double(count) / Double(trials)
+    let formattedPercentage = formatter.string(from: percentage as NSNumber)!
+    print("\t\(result): \(count)\t\(formattedPercentage)")
 }
